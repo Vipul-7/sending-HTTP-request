9f7d0390e3f556f219994960ed8dd5f4e586805e
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-06 10:31:47 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMoviesHanlder = () => {
+    fetch("https://swapi.dev/api/films")
+      .then((response) => response.json())
+      .then((data) => setMovies(data.results));
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
