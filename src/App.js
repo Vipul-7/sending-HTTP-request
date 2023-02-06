3d8188c915c17679d814a1280b0023d8a2f60a68
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-02-06 11:14:32 +0530

    sending get req.

--- src/App.js
+++ src/App.js
@@ -6,22 +6,40 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  const fetchMoviesHanlder = () => {
-    fetch("https://swapi.dev/api/films")
-      .then((response) => response.json())
-      .then((data) => {
-        const trandformedMovies = data.results.map((movieData) => {
-          return {
-            id: movieData.episode_id,
-            title: movieData.title,
-            openingText: movieData.opening_crawl,
-            release: movieData.release_date,
-          };
-        });
+  // const fetchMoviesHanlder = () => {
+  //   fetch("https://swapi.dev/api/films")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const trandformedMovies = data.results.map((movieData) => {
+  //         return {
+  //           id: movieData.episode_id,
+  //           title: movieData.title,
+  //           openingText: movieData.opening_crawl,
+  //           release: movieData.release_date,
+  //         };
+  //       });
 
-        setMovies(trandformedMovies);
-      });
+  //       setMovies(trandformedMovies);
+  //     });
+  // };
+
+  // Promises can be written in another way async and await
+  const fetchMoviesHanlder = async () => {
+    const response = await fetch("https://swapi.dev/api/films");
+    const data = await response.json();
+    const trandformedMovies = data.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        release: movieData.release_date,
+      };
+    });
+
+    setMovies(trandformedMovies);
   };
+
+
 
   return (
     <React.Fragment>
